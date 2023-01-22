0ad67a45eb5cc826d061f57b7d3db8057a5fca1f
Arpit Verma <arpitv970@gmail.com>
2023-01-22 13:48:22 +0530

    refactored UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import FetchMusic from './components/FetchMusic';
 import Navbar from './components/Navbar';
 
+import { Routes, Route } from 'react-router-dom';
+import Search from './components/Search';
+
 const App = () => {
     return (
         <div>
             <Navbar />
-            <FetchMusic />
+            <Routes>
+                <Route path='/' element={<FetchMusic />} />
+                <Route path='/search' element={<Search />} />
+            </Routes>
         </div>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,29 +2,106 @@ import { AiFillHome, AiOutlineSearch, AiFillStar } from 'react-icons/ai';
 import { IconContext } from 'react-icons/lib/esm/iconContext';
 import { RiPlayListLine } from 'react-icons/ri';
 
+import { NavLink } from 'react-router-dom';
+
+import { useState } from 'react';
+
 import './Navbar.css';
 import ToolTip from './ToolTip';
 
 const Navbar = () => {
+    const [isHover, setIsHover] = useState(false);
+    const [isHoverSearch, setIsHoverSearch] = useState(false);
+    const [isHoverFav, setIsHoverFav] = useState(false);
+    const [isHoverPlay, setIsHoverPlay] = useState(false);
     return (
         <nav className='container text-white position-fixed w-auto h-100 d-flex justify-content-center align-items-center'>
             <IconContext.Provider value={{ size: '2rem' }}>
-                <ul className='list-unstyled d-flex flex-column justify-content-evenly align-items-center text-center h-75'>
+                <ul className='list-unstyled d-flex flex-column justify-content-evenly align-items-center text-center h-100'>
                     <li>
-                        <AiFillHome />
-                        <ToolTip tip='Home' />
+                        <NavLink
+                            to='/'
+                            style={{
+                                textDecoration: 'none',
+                                color: 'rgb(130, 130, 130)',
+                            }}
+                            className={`nav_initial ${
+                                isHover == true ? 'nav_Hover' : ''
+                            } `}
+                            onMouseEnter={() => {
+                                setIsHover(true);
+                            }}
+                            onMouseLeave={() => {
+                                setIsHover(false);
+                            }}
+                        >
+                            <AiFillHome />
+                            <ToolTip tip='Home' />
+                        </NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink
+                            to='/search'
+                            style={{
+                                textDecoration: 'none',
+                                color: 'rgb(130, 130, 130)',
+                            }}
+                            className={`nav_initial ${
+                                isHoverSearch == true ? 'nav_Hover' : ''
+                            } `}
+                            onMouseEnter={() => {
+                                setIsHoverSearch(true);
+                            }}
+                            onMouseLeave={() => {
+                                setIsHoverSearch(false);
+                            }}
+                        >
+                            <AiOutlineSearch />
+                            <ToolTip tip='Search' />
+                        </NavLink>
                     </li>
                     <li>
-                        <AiOutlineSearch />
-                        <ToolTip tip='Search' />
+                        <NavLink
+                            to='/'
+                            style={{
+                                textDecoration: 'none',
+                                color: 'rgb(130, 130, 130)',
+                            }}
+                            className={`nav_initial ${
+                                isHoverFav == true ? 'nav_Hover' : ''
+                            } `}
+                            onMouseEnter={() => {
+                                setIsHoverFav(true);
+                            }}
+                            onMouseLeave={() => {
+                                setIsHoverFav(false);
+                            }}
+                        >
+                            <AiFillStar />
+                            <ToolTip tip='Favorite' />
+                        </NavLink>
                     </li>
                     <li>
-                        <AiFillStar />
-                        <ToolTip tip='Favorite' />
-                    </li>
-                    <li>
-                        <RiPlayListLine />
-                        <ToolTip tip='Playlists' />
+                        <NavLink
+                            to='/'
+                            style={{
+                                textDecoration: 'none',
+                                color: 'rgb(130, 130, 130)',
+                            }}
+                            className={`nav_initial ${
+                                isHoverPlay == true ? 'nav_Hover' : ''
+                            } `}
+                            onMouseEnter={() => {
+                                setIsHoverPlay(true);
+                            }}
+                            onMouseLeave={() => {
+                                setIsHoverPlay(false);
+                            }}
+                        >
+                            <RiPlayListLine />
+                            <ToolTip tip='Playlists' />
+                        </NavLink>
                     </li>
                 </ul>
             </IconContext.Provider>

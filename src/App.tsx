60c62c2a4cd123d0e9802d782c53b39feea132e6
Arpit Verma <arpitv970@gmail.com>
2023-01-25 17:49:48 +0530

    Stored music locally

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,14 @@ const options = {
     url: 'https://shazam.p.rapidapi.com/songs/list-recommendations',
     params: { key: '484129036', locale: 'en-US' },
     headers: {
-        'X-RapidAPI-Key': '9f9c71c4aemsh5e77d18ed69fabep1f2169jsn3f0d8839bfac',
+        'X-RapidAPI-Key': '6e5e8f2ab4msh88a66b64b12a8cbp1fd60bjsn5f10a02730c6',
         'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
     },
 };
 
 const App = () => {
     const [music, setMusic] = useState<any[]>([]);
+    const [musicDB, setMusicDB] = useState<any[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
@@ -22,13 +23,24 @@ const App = () => {
             await axios
                 .request(options)
                 .then((res) => {
-                    setMusic(res.data.tracks);
+                    setMusicDB(res.data.tracks);
+                    localStorage.setItem('muiscDB', JSON.stringify(musicDB));
                     setLoading(!loading);
                 })
                 .catch((err) => console.error(err));
         };
 
+        const items = JSON.parse(localStorage.getItem('musicDB') || '{}');
+
+        const fetchMusic = async () => {
+            setMusic(items);
+        };
+
         pullMusic();
+        if (items) {
+            fetchMusic();
+        }
+        
     }, []);
 
     return (

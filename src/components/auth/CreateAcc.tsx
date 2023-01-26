092f75d851619f88fd2e245278f623d97034aa0c
Arpit Verma <arpitv970@gmail.com>
2023-01-26 16:56:24 +0530

    Locally added  user

--- src/components/auth/CreateAcc.tsx
+++ src/components/auth/CreateAcc.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header from '../Header';
+import { User } from '../Interfaces/User';
 
 const CreateAcc = () => {
     const [userName, setUserName] = useState('');
@@ -7,16 +8,55 @@ const CreateAcc = () => {
     const [pass, setPass] = useState('');
     const [confirmPass, setConfirmPass] = useState('');
 
+    const user = {
+        userName,
+        email,
+        pass,
+    };
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        if (!userName) {
+            return alert('Enter User Name');
+        }
+        if (!email) {
+            return alert('Enter Email');
+        }
+        if (!pass) {
+            return alert('Enter Password');
+        }
+        if (!confirmPass) {
+            return alert('Re-enter Password');
+        }
+
+        if (pass != confirmPass) {
+            return alert('Re-enter correct Password');
+        }
+
+        setUserName(userName);
+        setEmail(email);
+        setPass(pass);
+
+        localStorage.setItem('usersDB', JSON.stringify(user));
+        return alert('Acc Created!');
+    };
+
     return (
         <div>
             <Header title='Create Your Account' />
-            <form className='d-flex flex-column justify-content-center m-auto align-items-center'>
+            <form
+                onSubmit={handleSubmit}
+                className='d-flex flex-column justify-content-center m-auto align-items-center'
+            >
                 <div className='user-name mb-3 w-100'>
                     <label>Full Name</label>
                     <input
                         placeholder='Enter Full Name'
                         type='text'
                         className='form-control border border-secondary'
+                        onChange={(e) => {
+                            setUserName(e.target.value);
+                        }}
                     />
                 </div>
                 <div className='user-email mb-3 w-100'>
@@ -26,6 +66,9 @@ const CreateAcc = () => {
                         type='email'
                         className='form-control border border-secondary'
                         aria-describedby='emailHelp'
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
                     />
                 </div>
                 <div className='user-pass mb-3 w-100'>
@@ -35,6 +78,9 @@ const CreateAcc = () => {
                         type='password'
                         className='form-control border border-secondary'
                         aria-describedby='passwordHelpBlock'
+                        onChange={(e) => {
+                            setPass(e.target.value);
+                        }}
                     />
                 </div>
                 <div className='user-pass mb-3 w-100'>
@@ -44,6 +90,9 @@ const CreateAcc = () => {
                         type='password'
                         className='form-control border border-secondary'
                         aria-describedby='passwordHelpBlock'
+                        onChange={(e) => {
+                            setConfirmPass(e.target.value);
+                        }}
                     />
                 </div>
                 <button

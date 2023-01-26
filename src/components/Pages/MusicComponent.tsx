4cc0154bea21d3e1de279755de067d230df7ac98
Arpit Verma <arpitv970@gmail.com>
2023-01-26 14:03:39 +0530

    Revert "added fav and playlist icon to each music component"
    This reverts commit b214873916f944760d9ebffa408c582cb7ff3c9e.

--- src/components/Pages/MusicComponent.tsx
+++ src/components/Pages/MusicComponent.tsx
@@ -4,9 +4,6 @@ import { MusicPage } from '../Interfaces/MusicPage';
 import { BsSpotify } from 'react-icons/bs';
 import { SiItunes, SiDeezer } from 'react-icons/si';
 import { IconContext } from 'react-icons';
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
-import { RiPlayListLine } from 'react-icons/ri';
-import { useState } from 'react';
 
 const MusicComponent: React.FC<MusicPage> = ({
     title,
@@ -14,31 +11,15 @@ const MusicComponent: React.FC<MusicPage> = ({
     bgImg,
     mainImg,
 }) => {
-    const [isHover, setIsHover] = useState(false);
     return (
-        <IconContext.Provider value={{ size: '2rem' }}>
-            <div className='music-component'>
-                <Header title={title} />
-                <img className='music-component__img' src={mainImg} />
-                <section className='music-component__actions'>
-                    <span className='music-component__icons'>
-                        <span
-                            onMouseEnter={() => setIsHover(true)}
-                            onMouseLeave={() => setIsHover(false)}
-                        >
-                            {isHover === true ? (
-                                <AiFillStar />
-                            ) : (
-                                <AiOutlineStar />
-                            )}
-                        </span>
-                        <RiPlayListLine />
-                    </span>
-                    <h2 className='music-component__subtitle'>{subtitle}</h2>
-                </section>
-                <section className='music-component__hubs'>
-                    <h3 className='text-center'>Listen at</h3>
-                    <section className='music-component__provider'>
+        <div className='music-component'>
+            <Header title={title} />
+            <img className='music-component__img' src={mainImg} />
+            <h2 className='music-component__subtitle'>{subtitle}</h2>
+            <section className='music-component__hubs'>
+                <h3 className='text-center'>Listen at</h3>
+                <section className='music-component__provider'>
+                    <IconContext.Provider value={{size: '2rem'}}>
                         <span>
                             <SiItunes />
                             <p>Itunes</p>
@@ -51,10 +32,10 @@ const MusicComponent: React.FC<MusicPage> = ({
                             <SiDeezer />
                             <p>Deezer</p>
                         </span>
-                    </section>
+                    </IconContext.Provider>
                 </section>
-            </div>
-        </IconContext.Provider>
+            </section>
+        </div>
     );
 };
 
